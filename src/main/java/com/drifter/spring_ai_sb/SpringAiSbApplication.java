eefa7bcad346d9b8d9f476c952f20560db4b41e0
DonMB77 <purposefuldrifter@gmail.com>
2025-01-30 13:28:22 +0100

    initial init

---
+++ src/main/java/com/drifter/spring_ai_sb/SpringAiSbApplication.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_ai_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drifter/spring_ai_sb/SpringAiSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_ai_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
